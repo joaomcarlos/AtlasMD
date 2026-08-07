b59cd5194eab3f1ebb0a0cd9fd6822b16f502986
Joao Carlos <thesilentwarrior@gmail.com>
2026-08-07 16:51:52 +0100

    fix: stronger font for darkmode

--- atlasmd-renderer/plugins/fonts.ts
+++ atlasmd-renderer/plugins/fonts.ts
@@ -1,4 +1,5 @@
 import '@fontsource/eb-garamond/400.css'
+import '@fontsource/eb-garamond/500.css'
 import '@fontsource/eb-garamond/700.css'
 import '@fontsource/fira-code/400.css'
 import '@fontsource/fira-code/700.css'
